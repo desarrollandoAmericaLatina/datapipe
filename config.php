afce2f9c8b4c8debbf2c2762909a72e75318463a
Matías Parodi <mparodilabs@gmail.com>
2011-12-05 12:26:52 -0200

    Un bug menos en la presentación!

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ if (!defined('UNDER_INDEX')) {
 $GLOBALS['DB']['host'] = 'localhost';
 $GLOBALS['DB']['port'] = '5432';
 $GLOBALS['DB']['username'] = 'postgres';
-$GLOBALS['DB']['password'] = 'postgrespass123';
+$GLOBALS['DB']['password'] = 'postgres';
 $GLOBALS['DB']['dbname'] = 'datapipe';
 
 ?>
